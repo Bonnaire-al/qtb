41d5187dafdacb368ec909ee58670ef7166c7d87
zeus <dev.58@outlook.fr>
2026-02-23 13:07:37 +0100

    finish2

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,9 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.join(__dirname, '..', 'data', 'database.db');
+const dbPath = process.env.NODE_ENV === 'production'
+  ? '/data/database.db'
+  : path.join(__dirname, '..', 'data', 'database.db');
 
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
